5427c8cca04942289f1cddbd5674ca5bc63d56c2
GabrielHuanacoImpa <gabrielhuanaco@gmail.com>
2022-10-10 20:51:08 -0300

    Lista de items

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+import {Task} from '../../Task' //interface
+import {TASKS} from '../../mock-tasks'; //lista de tareas
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+  tasks: Task[] = TASKS;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
